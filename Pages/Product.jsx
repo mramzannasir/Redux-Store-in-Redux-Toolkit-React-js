86e506aba1cf7c70398bf0c1a24233e1f1f444fd
Muhammad Ramzan N <87058400+mramzannasir@users.noreply.github.com>
2022-09-25 15:11:31 +0500

    Update Product.jsx

--- Pages/Product.jsx
+++ Pages/Product.jsx
@@ -7,7 +7,7 @@ const Product = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchProducts = async () => {
-      const res = await fetch("https://fakestoreapi.com/products");
+      const res = await fetch("https://fakestoreapi.com/products"); //if api not working then please change it 
       const data = await res.json();
       setProducts(data);
       
